f08e65b838622e458cee8a6a09224852fa671450
r221-lambda <shobhit@cmu.edu>
2024-04-05 19:32:25 -0400

    collect training data

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-#pyorbbecsdk
-export PYTHONPATH=$PYTHONPATH:$(pwd)/camera/orbbec/pyorbbecsdk/install/lib/ # DON'T forget do this
+export PYTHONPATH=$PYTHONPATH:$(pwd)/camera/orbbec/pyorbbecsdk/install/lib/
