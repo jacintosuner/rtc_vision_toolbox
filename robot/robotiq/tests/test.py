c77e6bf78eebae2e420ec33d8f2bac8218a3470e
r221-lambda <shobhit@cmu.edu>
2024-04-12 16:29:45 -0400

    add gripper module

---
+++ robot/robotiq/tests/test.py
@@ -0,0 +1,20 @@
+from robot.robotiq.robotiq_gripper import RobotiqGripper
+
+#Test
+if True:
+    grip=RobotiqGripper("/dev/ttyUSB0")
+    print("init done...")
+    
+    input("Press Enter to continue...")
+    
+    grip.closeGripper()
+    
+    input("Press Enter to continue...")
+
+    grip.openGripper()
+
+    input("Press Enter to continue...")
+    
+    grip.calibrate(0,40)
+    grip.goTomm(20,255,255)
+    grip.goTomm(40,1,255)
