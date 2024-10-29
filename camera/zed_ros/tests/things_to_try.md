4a38b67e8c3bad4e494debdc27d1d9c7e2035684
Shobhit Aggarwal <shobhita@andrew.cmu.edu>
2024-10-29 09:16:45 -0400

    revised main from dev

--- camera/zed_ros/tests/things_to_try.md
+++
@@ -1,3 +0,0 @@
-1. default point cloud
-2. change resolution to 320x736
-3. rotate the disparity to have similar height in the same row
--- scripts/unused_scripts/test.py
+++
@@ -1,7 +0,0 @@
-import hydra
-import numpy as np
-import omegaconf
-
-
-@hydra.main(config_path="../models/taxpose/configs", config_name="eval_mfi")
-class 
