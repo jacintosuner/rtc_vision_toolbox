4341234bc8a540064bb9fa7e174bd39be2a28b54
Shobhit Aggarwal <shobhita@andrew.cmu.edu>
2024-10-30 14:01:43 -0400

    Dev (#6)
    * added demo-example
    
    * added README notes
    
    * updated installation steps
    
    * initial list of requirements.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
+
 setup(
-    name="rtc_vision_toolbox",
-    version="1.0",
-    packages=find_packages(),
+    name='rtc_vision_toolbox',
+    version='1.0',
+    packages=find_packages(),  # Automatically find all packages (e.g., 'core')
+    install_requires=[         # Add dependencies from requirements.txt
+        line.strip() for line in open('requirements.txt').readlines()
+    ]
 )
