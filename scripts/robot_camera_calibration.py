af0d8bed19a3ebf351745fdf1aefd2d0d107a32b
Jacinto <jacinto@lambdavector.wifi.local.cmu.edu>
2024-11-25 15:34:59 -0500

    local configuration

--- scripts/robot_camera_calibration.py
+++ scripts/robot_camera_calibration.py
@@ -1,36 +1,45 @@
-import argparse
 from calibration.calibrations import *
 from calibration.marker.aruco_marker import ArucoMarker
-from camera.orbbec.ob_camera import OBCamera
-from camera.zed_ros.zed_ros import ZedRos
-from robot.ros_robot.ros_robot import ROSRobot
+from camera.kinect.kinect import KinectCamera
+from robot.franka.franka import FrankaRobot
 
 
 def robot_camera_calibration():
 
-    camera = ZedRos(camera_node=f'/cam2/zed_cam2', camera_type='zedxm', rosmaster_ip='localhost')
+    # camera = ZedRos(camera_node=f'/cam2/zed_cam2', camera_type='zedxm', rosmaster_ip='localhost')
+    camera = KinectCamera()
 
     print("=====================================")
     print("CAMERA INITIALIZED")
     print("=====================================")
 
-    marker = ArucoMarker(type='DICT_4X4_100', size=0.05)
+    # marker = ArucoMarker(type='DICT_4X4_100', size=0.05)
+    marker = ArucoMarker(type='DICT_ARUCO_ORIGINAL', size=0.048, debug=True)
 
     print("=====================================")
     print("MARKER INITIALIZED")
     print("=====================================")
 
-    robot = ROSRobot(robot_name='yk_builder', rosmaster_ip='172.26.179.142')
+    # robot = ROSRobot(robot_name='yk_builder', rosmaster_ip='172.26.179.142')
+    robot = FrankaRobot()
 
     print("=====================================")
     print("ROBOT INITIALIZED")
     print("=====================================")
 
+    # T_eef2marker = np.array(
+    #     [
+    #         [0.0, 0.0, 1.0, 0.041502],
+    #         [-1.0, 0.0, 0.0, 0.0],
+    #         [0.0, -1.0, 0.0, 0.080824],
+    #         [0.0, 0.0, 0.0, 1.0],
+    #     ]
+    # )
     T_eef2marker = np.array(
         [
-            [0.0, 0.0, 1.0, 0.041502],
-            [-1.0, 0.0, 0.0, 0.0],
-            [0.0, -1.0, 0.0, 0.080824],
+            [1.0, 0.0, 0.0, 0.048914],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, -0.03503],
             [0.0, 0.0, 0.0, 1.0],
         ]
     )
